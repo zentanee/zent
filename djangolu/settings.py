c3ccc26a00786952605b3e5bf78734b707782109
vyaesop <100709552+vyaesop@users.noreply.github.com>
2023-08-08 16:43:24 +0300

    Update settings.py

--- djangolu/settings.py
+++ djangolu/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'staticfiles'),
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 
