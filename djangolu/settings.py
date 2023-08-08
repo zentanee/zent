c1a3178bbf359fdc656694f8a8abd8d03dec0d13
vyaesop <100709552+vyaesop@users.noreply.github.com>
2023-08-08 10:55:49 +0300

    Update settings.py

--- djangolu/settings.py
+++ djangolu/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
